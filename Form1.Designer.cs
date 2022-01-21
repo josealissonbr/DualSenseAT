45ad1fdec3231d21a5ef503b08df25ba148b1717
josealissonbr <98172458+josealissonbr@users.noreply.github.com>
2022-01-21 19:47:10 -0300

    Update Code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,8 +31,14 @@ namespace DualSenseAT
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.gameList = new System.Windows.Forms.ComboBox();
             this.OnTime = new System.Windows.Forms.Timer(this.components);
+            this.gameProcessNameLabel = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.procOpenLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.procIdLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -44,32 +50,111 @@ namespace DualSenseAT
             this.label1.TabIndex = 1;
             this.label1.Text = "Selecione o game";
             // 
-            // comboBox1
+            // gameList
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.gameList.FormattingEnabled = true;
+            this.gameList.Items.AddRange(new object[] {
             "Resident Evil 5 (Steam)"});
-            this.comboBox1.Location = new System.Drawing.Point(175, 147);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(195, 21);
-            this.comboBox1.TabIndex = 2;
+            this.gameList.Location = new System.Drawing.Point(175, 147);
+            this.gameList.Name = "gameList";
+            this.gameList.Size = new System.Drawing.Size(195, 21);
+            this.gameList.TabIndex = 2;
             // 
             // OnTime
             // 
+            this.OnTime.Enabled = true;
             this.OnTime.Interval = 200;
             this.OnTime.Tick += new System.EventHandler(this.OnTime_Tick);
+            // 
+            // gameProcessNameLabel
+            // 
+            this.gameProcessNameLabel.AutoSize = true;
+            this.gameProcessNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.gameProcessNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameProcessNameLabel.ForeColor = System.Drawing.Color.Black;
+            this.gameProcessNameLabel.Location = new System.Drawing.Point(150, 232);
+            this.gameProcessNameLabel.Name = "gameProcessNameLabel";
+            this.gameProcessNameLabel.Size = new System.Drawing.Size(29, 15);
+            this.gameProcessNameLabel.TabIndex = 43;
+            this.gameProcessNameLabel.Text = "N/A";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.BackColor = System.Drawing.Color.Transparent;
+            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.ForeColor = System.Drawing.Color.Black;
+            this.label22.Location = new System.Drawing.Point(3, 231);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(146, 15);
+            this.label22.TabIndex = 42;
+            this.label22.Text = "Game Process Name:";
+            // 
+            // procOpenLabel
+            // 
+            this.procOpenLabel.AutoSize = true;
+            this.procOpenLabel.BackColor = System.Drawing.Color.Transparent;
+            this.procOpenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.procOpenLabel.ForeColor = System.Drawing.Color.Black;
+            this.procOpenLabel.Location = new System.Drawing.Point(151, 274);
+            this.procOpenLabel.Name = "procOpenLabel";
+            this.procOpenLabel.Size = new System.Drawing.Size(29, 15);
+            this.procOpenLabel.TabIndex = 41;
+            this.procOpenLabel.Text = "N/A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(86, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "STATUS: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(69, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 15);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Process ID: ";
+            // 
+            // procIdLabel
+            // 
+            this.procIdLabel.AutoSize = true;
+            this.procIdLabel.BackColor = System.Drawing.Color.Transparent;
+            this.procIdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.procIdLabel.ForeColor = System.Drawing.Color.Black;
+            this.procIdLabel.Location = new System.Drawing.Point(152, 252);
+            this.procIdLabel.Name = "procIdLabel";
+            this.procIdLabel.Size = new System.Drawing.Size(15, 15);
+            this.procIdLabel.TabIndex = 38;
+            this.procIdLabel.Text = "#";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(543, 298);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.gameProcessNameLabel);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.procOpenLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.procIdLabel);
+            this.Controls.Add(this.gameList);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DualSenseAT by José Alisson";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -78,8 +163,14 @@ namespace DualSenseAT
 
         #endregion
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox gameList;
         private System.Windows.Forms.Timer OnTime;
+        private System.Windows.Forms.Label gameProcessNameLabel;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label procOpenLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label procIdLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -28,14 +30,130 @@ namespace DualSenseAT
         private bool validNumber = false;
         private int temporaryNumb = 0;
 
+        public struct Controller
+        {
+            public enum Types
+            {
+                Normal = 1,
+                Hard = 2,
+            }
+            public struct WriteController
+            {
+                public static void SetRightTrigger(Controller.Types type)
+                {
+                        var MyIni = new IniFile(@"C:\DualsenseX_GameTriggers.txt");
+
+                    switch (type)
+                    {
+                        case Controller.Types.Normal: 
+                            MyIni.Write("RightTrigger", "Normal");
+                            break;
+                        case Controller.Types.Hard:
+                            MyIni.Write("RightTrigger", "Hard");
+                            break;
+                    }
+                }
+            }
+        }
+
+
+        private void findProcessID()
+        {
+            processID = memLib.GetProcIdFromName("re5dx9"); //Gets process ID
+            processOpen = memLib.OpenProcess(processID);
+
+            if (processID > 0)
+            {
+                procIdLabel.Invoke((MethodInvoker)delegate
+                {
+                    procIdLabel.Text = processID.ToString();
+                    procIdLabel.ForeColor = Color.Lime;
+                });
+
+                gameProcessNameLabel.Invoke((MethodInvoker)delegate {
+                    gameProcessNameLabel.Text = "re5dx9.exe";
+                    gameProcessNameLabel.ForeColor = Color.Lime;
+                });
+            }
+            else
+            {
+                procIdLabel.Invoke((MethodInvoker)delegate
+                {
+                    procIdLabel.Text = "Process ID Not Found";
+                    procIdLabel.ForeColor = Color.Red;
+                });
+
+                gameProcessNameLabel.Invoke((MethodInvoker)delegate {
+                    gameProcessNameLabel.Text = "N/A";
+                    gameProcessNameLabel.ForeColor = Color.Red;
+                });
+            }
+
+            if (processOpen)
+            {
+
+                procOpenLabel.Invoke((MethodInvoker)delegate
+                {
+                    procOpenLabel.Text = "JOGO ENCONTRADO";
+                    procOpenLabel.ForeColor = Color.Lime;
+                });
+
+            }
+            else
+            {
+                procOpenLabel.Invoke((MethodInvoker)delegate
+                {
+                    procOpenLabel.Text = "N/A";
+                    procOpenLabel.ForeColor = Color.Red;
+                });
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            
 
+            findProcessID();
+
+            if (!processOpen)
+            {
+                MessageBox.Show("Falha ao encontrar o processo do jogo 're5dx9.exe'");
+                Thread.Sleep(1000);
+                return;
+            }
+        }
+
+        private void ResidentEvil5Steam()
+        {
+            var MyIni = new IniFile(@"C:\DualsenseX_GameTriggers.txt");
+
+            int ammo = memLib.ReadInt("re5dx9.exe+00DA2604,84,238,CE4,5C,36C,0,F40");
+            // MyIni.Write("RightTrigger", "Resistance");
+            MyIni.Write("LeftTrigger", "Resistance");
+            MyIni.Write("ForceLeftTrigger", "(4)(2)");
+
+            if (ammo > 0)
+            {
+                Controller.WriteController.SetRightTrigger(Controller.Types.Hard);
+              //  MyIni.Write("RightTrigger", "SemiAutomaticGun");
+               // MyIni.Write("ForceLeftTrigger", "(2)(5)(2)");
+            }
+            else
+                Controller.WriteController.SetRightTrigger(Controller.Types.Normal);
+
+            Console.WriteLine("municao: " + ammo);
         }
 
         private void OnTime_Tick(object sender, EventArgs e)
         {
+            if (!processOpen)
+                return;
+            if (gameList.SelectedIndex < 0)
+                return;
+            if (gameList.SelectedItem.ToString() == "Resident Evil 5 (Steam)")
+                ResidentEvil5Steam();
 
+            
         }
     }
 }
